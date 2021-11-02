826d7d3ef6725bf682a643f288f90ab259dd1cdf
Justin Handville <nanolith@gmail.com>
2021-11-01 20:28:05 -0400

    Added resource handle method.

---
+++ src/entity_cert/vcblockchain_entity_private_cert_resource_handle.c
@@ -0,0 +1,24 @@
+/**
+ * \file entity_cert/vcblockchain_entity_private_cert_resource_handle.c
+ *
+ * \brief Get the resource handle for the given entity certificate.
+ *
+ * \copyright 2021 Velo Payments, Inc.  All rights reserved.
+ */
+
+#include "entity_cert_internal.h"
+
+/**
+ * \brief Get the resource handle for the given private certificate.
+ *
+ * \param cert      The private certificate instance to access.
+ *
+ * \returns the resource handle for this certificate instance.
+ */
+RCPR_SYM(resource)* vcblockchain_entity_private_cert_resource_handle(
+    vcblockchain_entity_private_cert* cert)
+{
+    MODEL_ASSERT(prop_vcblockchain_entity_private_cert_valid(cert));
+
+    return &cert->pub.hdr;
+}
