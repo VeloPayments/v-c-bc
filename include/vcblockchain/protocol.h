e95fe0994e6cdfad723df57f29e1e565f9ae484e
Justin Handville <nanolith@gmail.com>
2021-11-12 03:45:18 -0500

    Added block prev id request send function.

--- include/vcblockchain/protocol.h
+++ include/vcblockchain/protocol.h
@@ -265,6 +265,34 @@ int vcblockchain_protocol_sendreq_block_next_id_get(
     vccrypt_buffer_t* shared_secret, uint32_t offset, const vpr_uuid* block_id);
 
 /**
+ * \brief Send a block get prev id request.
+ *
+ * \param sock                      The socket to which this request is written.
+ * \param suite                     The crypto suite to use for this handshake.
+ * \param client_iv                 Pointer to the client IV, updated by this
+ *                                  call.
+ * \param shared_secret             The shared secret key for this request.
+ * \param offset                    The offset to use for this request. It
+ *                                  should be unique per any outbound request
+ *                                  for which a response has not yet been
+ *                                  received.
+ * \param block_id                  The block UUID to get, or zero_uuid for the
+ *                                  first block, or 0xff uuid for last block.
+ *
+ * This function sends a block get prev id request to the server.
+ *
+ * \returns a status code indicating success or failure.
+ *      - VCBLOCKCHAIN_STATUS_SUCCESS on success.
+ *      - VCBLOCKCHAIN_ERROR_SSOCK_WRITE if writing to the socket failed.
+ *      - VCBLOCKCHAIN_ERROR_OUT_OF_MEMORY if this operation encountered an
+ *        out-of-memory error.
+ *      - a non-zero error response if something else has failed.
+ */
+int vcblockchain_protocol_sendreq_block_prev_id_get(
+    ssock* sock, vccrypt_suite_options_t* suite, uint64_t* client_iv,
+    vccrypt_buffer_t* shared_secret, uint32_t offset, const vpr_uuid* block_id);
+
+/**
  * \brief Receive a response from the API.
  *
  * \param sock                      The socket from which this response is read.
