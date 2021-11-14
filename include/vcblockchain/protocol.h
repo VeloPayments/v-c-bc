e96b40333133cff646700582188049402a7a5542
Justin Handville <nanolith@gmail.com>
2021-11-14 03:45:36 -0500

    Added artifact last txn id get protocol request.

--- include/vcblockchain/protocol.h
+++ include/vcblockchain/protocol.h
@@ -321,6 +321,34 @@ int vcblockchain_protocol_sendreq_artifact_first_txn_id_get(
     const vpr_uuid* artifact_id);
 
 /**
+ * \brief Send an artifact get last transaction id request.
+ *
+ * \param sock                      The socket to which this request is written.
+ * \param suite                     The crypto suite to use for this handshake.
+ * \param client_iv                 Pointer to the client IV, updated by this
+ *                                  call.
+ * \param shared_secret             The shared secret key for this request.
+ * \param offset                    The offset to use for this request. It
+ *                                  should be unique per any outbound request
+ *                                  for which a response has not yet been
+ *                                  received.
+ * \param artifact_id               The artifact UUID to get.
+ *
+ * This function sends an artifact get last transaction request to the server.
+ *
+ * \returns a status code indicating success or failure.
+ *      - VCBLOCKCHAIN_STATUS_SUCCESS on success.
+ *      - VCBLOCKCHAIN_ERROR_SSOCK_WRITE if writing to the socket failed.
+ *      - VCBLOCKCHAIN_ERROR_OUT_OF_MEMORY if this operation encountered an
+ *        out-of-memory error.
+ *      - a non-zero error response if something else has failed.
+ */
+int vcblockchain_protocol_sendreq_artifact_last_txn_id_get(
+    ssock* sock, vccrypt_suite_options_t* suite, uint64_t* client_iv,
+    vccrypt_buffer_t* shared_secret, uint32_t offset,
+    const vpr_uuid* artifact_id);
+
+/**
  * \brief Receive a response from the API.
  *
  * \param sock                      The socket from which this response is read.
