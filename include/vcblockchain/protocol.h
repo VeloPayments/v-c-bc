b92bdeb1acb5b3980d104ea7a3fba1a182dfc0c7
Justin Handville <nanolith@gmail.com>
2021-11-17 23:25:05 -0500

    Added txn prev id get API call.

--- include/vcblockchain/protocol.h
+++ include/vcblockchain/protocol.h
@@ -431,6 +431,33 @@ int vcblockchain_protocol_sendreq_txn_next_id_get(
     vccrypt_buffer_t* shared_secret, uint32_t offset, const vpr_uuid* txn_id);
 
 /**
+ * \brief Send a transaction get prev id request.
+ *
+ * \param sock                      The socket to which this request is written.
+ * \param suite                     The crypto suite to use for this handshake.
+ * \param client_iv                 Pointer to the client IV, updated by this
+ *                                  call.
+ * \param shared_secret             The shared secret key for this request.
+ * \param offset                    The offset to use for this request. It
+ *                                  should be unique per any outbound request
+ *                                  for which a response has not yet been
+ *                                  received.
+ * \param txn_id                    The txn UUID to query.
+ *
+ * This function sends a transaction get request to the server.
+ *
+ * \returns a status code indicating success or failure.
+ *      - VCBLOCKCHAIN_STATUS_SUCCESS on success.
+ *      - VCBLOCKCHAIN_ERROR_SSOCK_WRITE if writing to the socket failed.
+ *      - VCBLOCKCHAIN_ERROR_OUT_OF_MEMORY if this operation encountered an
+ *        out-of-memory error.
+ *      - a non-zero error response if something else has failed.
+ */
+int vcblockchain_protocol_sendreq_txn_prev_id_get(
+    ssock* sock, vccrypt_suite_options_t* suite, uint64_t* client_iv,
+    vccrypt_buffer_t* shared_secret, uint32_t offset, const vpr_uuid* txn_id);
+
+/**
  * \brief Receive a response from the API.
  *
  * \param sock                      The socket from which this response is read.
