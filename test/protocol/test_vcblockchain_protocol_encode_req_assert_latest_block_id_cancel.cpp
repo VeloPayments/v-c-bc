d07bb9afd16b8e00c93eef4d112224b1b5d9c0ed
Justin Handville <nanolith@gmail.com>
2022-06-16 11:28:52 -0400

    Added block assertion cancel request encoder.

---
+++ test/protocol/test_vcblockchain_protocol_encode_req_assert_latest_block_id_cancel.cpp
@@ -0,0 +1,84 @@
+/**
+ * \file
+ * test/protocol/test_vcblockchain_protocol_encode_req_latest_block_id_cancel.cpp
+ *
+ * Unit tests for encoding the latest block id assertion cancel request.
+ *
+ * \copyright 2022 Velo Payments, Inc.  All rights reserved.
+ */
+
+#include <arpa/inet.h>
+#include <gtest/gtest.h>
+#include <vcblockchain/error_codes.h>
+#include <vcblockchain/protocol/serialization.h>
+#include <vpr/allocator/malloc_allocator.h>
+
+using namespace std;
+
+/**
+ * This method should perform null checks on its pointer parameters.
+ */
+TEST(
+    test_vcblockchain_protocol_encode_req_assert_latest_block_id_cancel,
+    parameter_check)
+{
+    const uint32_t EXPECTED_OFFSET = 97;
+    allocator_options_t alloc_opts;
+    vccrypt_buffer_t buffer;
+
+    /* create an allocator instance. */
+    malloc_allocator_options_init(&alloc_opts);
+
+    /* This method performs null checks on pointer parameters. */
+    EXPECT_EQ(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG,
+        vcblockchain_protocol_encode_req_assert_latest_block_id_cancel(
+            nullptr, &alloc_opts, EXPECTED_OFFSET));
+    EXPECT_EQ(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG,
+        vcblockchain_protocol_encode_req_assert_latest_block_id_cancel(
+            &buffer, nullptr, EXPECTED_OFFSET));
+
+    /* clean up. */
+    dispose((disposable_t*)&alloc_opts);
+}
+
+/**
+ * If valid parameters are provided, this method encodes a request message.
+ */
+TEST(
+    test_vcblockchain_protocol_encode_req_assert_latest_block_id_cancel,
+    happy_path)
+{
+    const uint32_t EXPECTED_OFFSET = 97;
+    allocator_options_t alloc_opts;
+    vccrypt_buffer_t buffer;
+
+    /* create an allocator instance. */
+    malloc_allocator_options_init(&alloc_opts);
+
+    /* precondition: set the buffer to null / 0. */
+    buffer.data = nullptr; buffer.size = 0;
+
+    /* This method encodes the request. */
+    ASSERT_EQ(
+        VCBLOCKCHAIN_STATUS_SUCCESS,
+        vcblockchain_protocol_encode_req_assert_latest_block_id_cancel(
+            &buffer, &alloc_opts, EXPECTED_OFFSET));
+
+    /* compute the message size. */
+    size_t message_size = 2 * sizeof(uint32_t);
+
+    /* the buffer has been initialized. */
+    ASSERT_NE(nullptr, buffer.data);
+    ASSERT_EQ(message_size, buffer.size);
+
+    /* verify that the request id and offset are set correctly. */
+    const uint32_t* u32arr = (const uint32_t*)buffer.data;
+    EXPECT_EQ(htonl(PROTOCOL_REQ_ID_ASSERT_LATEST_BLOCK_ID), u32arr[0]);
+    EXPECT_EQ(htonl(EXPECTED_OFFSET), u32arr[1]);
+
+    /* clean up. */
+    dispose((disposable_t*)&buffer);
+    dispose((disposable_t*)&alloc_opts);
+}
