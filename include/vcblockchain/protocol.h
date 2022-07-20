5ea5f1cbde01eb75705dc887f2728f3e7a11aebf
Justin Handville <nanolith@gmail.com>
2022-07-20 05:42:08 -0400

    Made key parameter const in method.

--- include/vcblockchain/protocol.h
+++ include/vcblockchain/protocol.h
@@ -322,7 +322,7 @@ int vcblockchain_protocol_sendreq_block_prev_id_get(
  */
 int vcblockchain_protocol_sendreq_block_id_by_height_get(
     ssock* sock, vccrypt_suite_options_t* suite, uint64_t* client_iv,
-    vccrypt_buffer_t* shared_secret, uint32_t offset, uint64_t height);
+    const vccrypt_buffer_t* shared_secret, uint32_t offset, uint64_t height);
 
 /**
  * \brief Send an artifact get first transaction id request.
