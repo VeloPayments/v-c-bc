81356ff9b31526ed4abcd6f2fbfa1ada07b95ca8
Justin Handville <nanolith@gmail.com>
2023-08-04 17:16:09 -0400

    Extracted max encrypted packet size constant.

---
+++ include/vcblockchain/limits.h
@@ -0,0 +1,16 @@
+/**
+ * \file vcblockchain/limits.h
+ *
+ * \brief API limits.
+ *
+ * \copyright 2023 Velo Payments, Inc.  All rights reserved.
+ */
+
+#ifndef VCBLOCKCHAIN_LIMITS_HEADER_GUARD
+#define VCBLOCKCHAIN_LIMITS_HEADER_GUARD
+
+/* Set the maximum encrypted packet size to 250 MB. */
+#define VCBLOCKCHAIN_LIMIT_MAXIMUM_ENCRYPTED_PACKET_SIZE \
+    (250ULL * 1024ULL * 1024ULL)
+
+#endif /*VCBLOCKCHAIN_LIMITS_HEADER_GUARD*/
