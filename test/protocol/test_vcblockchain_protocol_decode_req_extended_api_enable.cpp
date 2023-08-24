6c62fb91c1b02ac04713e1c0200d0ea79fc04e28
Justin Handville <nanolith@gmail.com>
2023-08-23 23:20:40 -0400

    Converted unit tests to minunit.

--- test/protocol/test_vcblockchain_protocol_decode_req_extended_api_enable.cpp
+++ test/protocol/test_vcblockchain_protocol_decode_req_extended_api_enable.cpp
@@ -4,60 +4,59 @@
  *
  * Unit tests for decoding an extended API enable request.
  *
- * \copyright 2022 Velo Payments, Inc.  All rights reserved.
+ * \copyright 2022-2023 Velo Payments, Inc.  All rights reserved.
  */
 
+#include <cstring>
+#include <minunit/minunit.h>
 #include <vcblockchain/error_codes.h>
 #include <vcblockchain/protocol/serialization.h>
 #include <vpr/allocator/malloc_allocator.h>
 
-/* DISABLED GTEST */
-#if 0
-
 using namespace std;
+
+TEST_SUITE(test_vcblockchain_protocol_decode_req_extended_api_enable);
 
 /**
  * This method should perform null checks on its pointer parameters.
  */
-TEST(
-    test_vcblockchain_protocol_decode_req_extended_api_enable,
-    parameter_checks)
+TEST(parameter_checks)
 {
     const uint8_t EXPECTED_PAYLOAD[4] = { 0x00, 0x01, 0x02, 0x03 };
     size_t EXPECTED_PAYLOAD_SIZE = sizeof(EXPECTED_PAYLOAD);
     protocol_req_extended_api_enable req;
 
     /* This method performs null checks on pointer parameters. */
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_decode_req_extended_api_enable(
-            nullptr, EXPECTED_PAYLOAD, EXPECTED_PAYLOAD_SIZE));
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_decode_req_extended_api_enable(
-            &req, nullptr, EXPECTED_PAYLOAD_SIZE));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_decode_req_extended_api_enable(
+                    nullptr, EXPECTED_PAYLOAD, EXPECTED_PAYLOAD_SIZE));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_decode_req_extended_api_enable(
+                    &req, nullptr, EXPECTED_PAYLOAD_SIZE));
 }
 
 /**
  * This method should verify the payload size.
  */
-TEST(test_vcblockchain_protocol_decode_req_extended_api_enable, payload_size)
+TEST(payload_size)
 {
     const uint8_t EXPECTED_PAYLOAD[4] = { 0x00, 0x01, 0x02, 0x03 };
     size_t EXPECTED_PAYLOAD_SIZE = sizeof(EXPECTED_PAYLOAD);
     protocol_req_extended_api_enable req;
 
     /* This method verifies the payload size. */
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_decode_req_extended_api_enable(
-            &req, EXPECTED_PAYLOAD, EXPECTED_PAYLOAD_SIZE));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_decode_req_extended_api_enable(
+                    &req, EXPECTED_PAYLOAD, EXPECTED_PAYLOAD_SIZE));
 }
 
 /**
  * This method can decode a properly encoded request message.
  */
-TEST(test_vcblockchain_protocol_decode_req_extended_api_enable, happy_path)
+TEST(happy_path)
 {
     const uint32_t EXPECTED_OFFSET = 21;
     allocator_options_t alloc_opts;
@@ -68,28 +67,27 @@ TEST(test_vcblockchain_protocol_decode_req_extended_api_enable, happy_path)
     malloc_allocator_options_init(&alloc_opts);
 
     /* we can encode a message. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vcblockchain_protocol_encode_req_extended_api_enable(
-            &buffer, &alloc_opts, EXPECTED_OFFSET));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vcblockchain_protocol_encode_req_extended_api_enable(
+                    &buffer, &alloc_opts, EXPECTED_OFFSET));
 
     /* precondition: the request buffer is zeroed out. */
     memset(&req, 0, sizeof(req));
 
     /* We can decode this message. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vcblockchain_protocol_decode_req_extended_api_enable(
-            &req, buffer.data, buffer.size));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vcblockchain_protocol_decode_req_extended_api_enable(
+                    &req, buffer.data, buffer.size));
 
     /* the request id is set correctly. */
-    EXPECT_EQ(PROTOCOL_REQ_ID_EXTENDED_API_ENABLE, req.request_id);
+    TEST_EXPECT(PROTOCOL_REQ_ID_EXTENDED_API_ENABLE == req.request_id);
     /* the offset is set correctly. */
-    EXPECT_EQ(EXPECTED_OFFSET, req.offset);
+    TEST_EXPECT(EXPECTED_OFFSET == req.offset);
 
     /* clean up. */
     dispose((disposable_t*)&req);
     dispose((disposable_t*)&buffer);
     dispose((disposable_t*)&alloc_opts);
 }
-#endif
