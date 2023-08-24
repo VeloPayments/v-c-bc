c8ce06a337a4d5f3774d9bb5694e8aea35256278
Justin Handville <nanolith@gmail.com>
2023-08-24 02:28:47 -0400

    Converted unit tests to minunit.

--- test/protocol/test_vcblockchain_protocol_decode_req_transaction_submit.cpp
+++ test/protocol/test_vcblockchain_protocol_decode_req_transaction_submit.cpp
@@ -4,22 +4,23 @@
  *
  * Unit tests for decoding the transaction submit request.
  *
- * \copyright 2020 Velo Payments, Inc.  All rights reserved.
+ * \copyright 2020-2023 Velo Payments, Inc.  All rights reserved.
  */
 
+#include <cstring>
+#include <minunit/minunit.h>
 #include <vcblockchain/error_codes.h>
 #include <vcblockchain/protocol/serialization.h>
 #include <vpr/allocator/malloc_allocator.h>
 
-/* DISABLED GTEST */
-#if 0
-
 using namespace std;
+
+TEST_SUITE(test_vcblockchain_protocol_decode_req_transaction_submit);
 
 /**
  * This method should perform null checks on its pointer parameters.
  */
-TEST(test_vcblockchain_protocol_decode_req_transaction_submit, parameter_check)
+TEST(parameter_check)
 {
     const uint8_t EXPECTED_PAYLOAD[4] = { 0x00, 0x01, 0x02, 0x03 };
     size_t EXPECTED_PAYLOAD_SIZE = sizeof(EXPECTED_PAYLOAD);
@@ -30,18 +31,19 @@ TEST(test_vcblockchain_protocol_decode_req_transaction_submit, parameter_check)
     malloc_allocator_options_init(&alloc_opts);
 
     /* This method performs null checks on pointer parameters. */
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_decode_req_transaction_submit(
-            nullptr, &alloc_opts, EXPECTED_PAYLOAD, EXPECTED_PAYLOAD_SIZE));
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_decode_req_transaction_submit(
-            &req, nullptr, EXPECTED_PAYLOAD, EXPECTED_PAYLOAD_SIZE));
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_decode_req_transaction_submit(
-            &req, &alloc_opts, nullptr, EXPECTED_PAYLOAD_SIZE));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_decode_req_transaction_submit(
+                    nullptr, &alloc_opts, EXPECTED_PAYLOAD,
+                    EXPECTED_PAYLOAD_SIZE));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_decode_req_transaction_submit(
+                    &req, nullptr, EXPECTED_PAYLOAD, EXPECTED_PAYLOAD_SIZE));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_decode_req_transaction_submit(
+                    &req, &alloc_opts, nullptr, EXPECTED_PAYLOAD_SIZE));
 
     /* clean up. */
     dispose((disposable_t*)&alloc_opts);
@@ -51,7 +53,7 @@ TEST(test_vcblockchain_protocol_decode_req_transaction_submit, parameter_check)
  * This method checks the payload size to make sure it's at least large enough
  * to hold the header data.
  */
-TEST(test_vcblockchain_protocol_decode_req_transaction_submit, payload_size)
+TEST(payload_size)
 {
     const uint8_t EXPECTED_PAYLOAD[4] = { 0x00, 0x01, 0x02, 0x03 };
     size_t EXPECTED_PAYLOAD_SIZE = sizeof(EXPECTED_PAYLOAD);
@@ -62,10 +64,11 @@ TEST(test_vcblockchain_protocol_decode_req_transaction_submit, payload_size)
     malloc_allocator_options_init(&alloc_opts);
 
     /* This method performs null checks on pointer parameters. */
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_decode_req_transaction_submit(
-            &req, &alloc_opts, EXPECTED_PAYLOAD, EXPECTED_PAYLOAD_SIZE));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_decode_req_transaction_submit(
+                    &req, &alloc_opts, EXPECTED_PAYLOAD,
+                    EXPECTED_PAYLOAD_SIZE));
 
     /* clean up. */
     dispose((disposable_t*)&alloc_opts);
@@ -74,7 +77,7 @@ TEST(test_vcblockchain_protocol_decode_req_transaction_submit, payload_size)
 /**
  * This method can decode a properly encoded request message.
  */
-TEST(test_vcblockchain_protocol_decode_req_transaction_submit, happy_path)
+TEST(happy_path)
 {
     const uint32_t EXPECTED_OFFSET = 88;
     const vpr_uuid EXPECTED_TXN_ID = { .data = {
@@ -93,36 +96,35 @@ TEST(test_vcblockchain_protocol_decode_req_transaction_submit, happy_path)
     malloc_allocator_options_init(&alloc_opts);
 
     /* we can encode a message. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vcblockchain_protocol_encode_req_transaction_submit(
-            &buffer, &alloc_opts, EXPECTED_OFFSET, &EXPECTED_TXN_ID,
-            &EXPECTED_ARTIFACT_ID, EXPECTED_CERT, EXPECTED_CERT_SIZE));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vcblockchain_protocol_encode_req_transaction_submit(
+                    &buffer, &alloc_opts, EXPECTED_OFFSET, &EXPECTED_TXN_ID,
+                    &EXPECTED_ARTIFACT_ID, EXPECTED_CERT, EXPECTED_CERT_SIZE));
 
     /* precondition: the request buffer is zeroed out. */
     memset(&req, 0, sizeof(req));
 
     /* we can decode this message. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vcblockchain_protocol_decode_req_transaction_submit(
-            &req, &alloc_opts, buffer.data, buffer.size));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vcblockchain_protocol_decode_req_transaction_submit(
+                    &req, &alloc_opts, buffer.data, buffer.size));
 
     /* the request id is set correctly. */
-    EXPECT_EQ(PROTOCOL_REQ_ID_TRANSACTION_SUBMIT, req.request_id);
+    TEST_EXPECT(PROTOCOL_REQ_ID_TRANSACTION_SUBMIT == req.request_id);
     /* the offset id is set correctly. */
-    EXPECT_EQ(EXPECTED_OFFSET, req.offset);
+    TEST_EXPECT(EXPECTED_OFFSET == req.offset);
     /* the transaction id is set correctly. */
-    EXPECT_EQ(0, memcmp(&req.txn_id, &EXPECTED_TXN_ID, 16));
+    TEST_EXPECT(0 == memcmp(&req.txn_id, &EXPECTED_TXN_ID, 16));
     /* the artifact id is set correctly. */
-    EXPECT_EQ(0, memcmp(&req.artifact_id, &EXPECTED_ARTIFACT_ID, 16));
+    TEST_EXPECT(0 == memcmp(&req.artifact_id, &EXPECTED_ARTIFACT_ID, 16));
     /* the certificate is set correctly. */
-    ASSERT_EQ(EXPECTED_CERT_SIZE, req.cert.size);
-    EXPECT_EQ(0, memcmp(req.cert.data, EXPECTED_CERT, EXPECTED_CERT_SIZE));
+    TEST_ASSERT(EXPECTED_CERT_SIZE == req.cert.size);
+    TEST_EXPECT(0 == memcmp(req.cert.data, EXPECTED_CERT, EXPECTED_CERT_SIZE));
 
     /* clean up. */
     dispose((disposable_t*)&req);
     dispose((disposable_t*)&buffer);
     dispose((disposable_t*)&alloc_opts);
 }
-#endif
