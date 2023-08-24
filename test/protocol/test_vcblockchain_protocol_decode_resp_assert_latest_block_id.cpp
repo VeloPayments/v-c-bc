b36a075a301f53d524fe0670928f8d2c12c51faf
Justin Handville <nanolith@gmail.com>
2023-08-24 02:50:59 -0400

    Converted unit tests to minunit.

--- test/protocol/test_vcblockchain_protocol_decode_resp_assert_latest_block_id.cpp
+++ test/protocol/test_vcblockchain_protocol_decode_resp_assert_latest_block_id.cpp
@@ -4,59 +4,59 @@
  *
  * Unit tests for decoding a latest block id assertion response.
  *
- * \copyright 2022 Velo Payments, Inc.  All rights reserved.
+ * \copyright 2022-2023 Velo Payments, Inc.  All rights reserved.
  */
 
+#include <cstring>
+#include <minunit/minunit.h>
 #include <vcblockchain/error_codes.h>
 #include <vcblockchain/protocol/serialization.h>
 #include <vpr/allocator/malloc_allocator.h>
 
-/* DISABLED GTEST */
-#if 0
-
 using namespace std;
+
+TEST_SUITE(test_vcblockchain_protocol_decode_resp_assert_latest_block_id);
 
 /**
  * This method should perform null checks on its pointer parameters.
  */
-TEST(test_vcblockchain_protocol_decode_resp_assert_latest_block_id, parameters)
+TEST(parameters)
 {
     const uint8_t EXPECTED_PAYLOAD[4] = { 0x00, 0x01, 0x02, 0x03 };
     size_t EXPECTED_PAYLOAD_SIZE = sizeof(EXPECTED_PAYLOAD);
     protocol_resp_assert_latest_block_id resp;
 
     /* This method performs null checks on pointer parameters. */
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_decode_resp_assert_latest_block_id(
-            nullptr, EXPECTED_PAYLOAD, EXPECTED_PAYLOAD_SIZE));
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_decode_resp_assert_latest_block_id(
-            &resp, nullptr, EXPECTED_PAYLOAD_SIZE));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_decode_resp_assert_latest_block_id(
+                    nullptr, EXPECTED_PAYLOAD, EXPECTED_PAYLOAD_SIZE));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_decode_resp_assert_latest_block_id(
+                    &resp, nullptr, EXPECTED_PAYLOAD_SIZE));
 }
 
 /**
  * This method should check the payload size to make sure it is correct.
  */
-TEST(
-    test_vcblockchain_protocol_decode_resp_assert_latest_block_id, payload_size)
+TEST(payload_size)
 {
     const uint8_t EXPECTED_PAYLOAD[4] = { 0x00, 0x01, 0x02, 0x03 };
     size_t EXPECTED_PAYLOAD_SIZE = sizeof(EXPECTED_PAYLOAD);
     protocol_resp_assert_latest_block_id resp;
 
     /* This method performs a payload size check. */
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_decode_resp_assert_latest_block_id(
-            &resp, EXPECTED_PAYLOAD, EXPECTED_PAYLOAD_SIZE));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_decode_resp_assert_latest_block_id(
+                    &resp, EXPECTED_PAYLOAD, EXPECTED_PAYLOAD_SIZE));
 }
 
 /**
  * This method can decode a properly encoded response message.
  */
-TEST(test_vcblockchain_protocol_decode_resp_assert_latest_block_id, happy_path)
+TEST(happy_path)
 {
     const uint32_t EXPECTED_OFFSET = 12;
     const uint32_t EXPECTED_STATUS = 77;
@@ -68,30 +68,29 @@ TEST(test_vcblockchain_protocol_decode_resp_assert_latest_block_id, happy_path)
     malloc_allocator_options_init(&alloc_opts);
 
     /* we can encode a message. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vcblockchain_protocol_encode_resp_assert_latest_block_id(
-            &buffer, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vcblockchain_protocol_encode_resp_assert_latest_block_id(
+                    &buffer, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS));
 
     /* precondition: the response buffer is zeroed out. */
     memset(&resp, 0, sizeof(resp));
 
     /* we can decode this message. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vcblockchain_protocol_decode_resp_assert_latest_block_id(
-            &resp, buffer.data, buffer.size));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vcblockchain_protocol_decode_resp_assert_latest_block_id(
+                    &resp, buffer.data, buffer.size));
 
     /* the request id is set correctly. */
-    EXPECT_EQ(PROTOCOL_REQ_ID_ASSERT_LATEST_BLOCK_ID, resp.request_id);
+    TEST_EXPECT(PROTOCOL_REQ_ID_ASSERT_LATEST_BLOCK_ID == resp.request_id);
     /* the offset is set correctly. */
-    EXPECT_EQ(EXPECTED_OFFSET, resp.offset);
+    TEST_EXPECT(EXPECTED_OFFSET == resp.offset);
     /* the status is set correctly. */
-    EXPECT_EQ(EXPECTED_STATUS, resp.status);
+    TEST_EXPECT(EXPECTED_STATUS == resp.status);
 
     /* clean up. */
     dispose((disposable_t*)&buffer);
     dispose((disposable_t*)&resp);
     dispose((disposable_t*)&alloc_opts);
 }
-#endif
