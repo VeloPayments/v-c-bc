3ce9c749be7f536902f40aace9d72ff8091f4914
Justin Handville <nanolith@gmail.com>
2023-08-24 19:10:35 -0400

    Converted unit tests to minunit.

--- test/protocol/test_vcblockchain_protocol_decode_resp_handshake_request.cpp
+++ test/protocol/test_vcblockchain_protocol_decode_resp_handshake_request.cpp
@@ -4,23 +4,24 @@
  *
  * Unit tests for decoding the response portion of the handshake request.
  *
- * \copyright 2020 Velo Payments, Inc.  All rights reserved.
+ * \copyright 2020-2023 Velo Payments, Inc.  All rights reserved.
  */
 
 #include <arpa/inet.h>
+#include <cstring>
+#include <minunit/minunit.h>
 #include <vcblockchain/protocol/data.h>
 #include <vcblockchain/protocol/serialization.h>
 #include <vpr/allocator/malloc_allocator.h>
 
-/* DISABLED GTEST */
-#if 0
-
 using namespace std;
+
+TEST_SUITE(test_vcblockchain_protocol_decode_resp_handshake_request);
 
 /**
  * Test the basics of the decoding.
  */
-TEST(test_vcblockchain_protocol_decode_resp_handshake_request, basics)
+TEST(basics)
 {
     allocator_options_t alloc_opts;
     vccrypt_suite_options_t suite;
@@ -43,109 +44,112 @@ TEST(test_vcblockchain_protocol_decode_resp_handshake_request, basics)
     malloc_allocator_options_init(&alloc_opts);
 
     /* create the crypto suite. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_options_init(&suite, &alloc_opts, VCCRYPT_SUITE_VELO_V1));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_options_init(
+                    &suite, &alloc_opts, VCCRYPT_SUITE_VELO_V1));
 
     /* create a buffer for holding the public key. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_cipher_key_agreement_public_key(
-            &suite, &server_public_key));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_cipher_key_agreement_public_key(
+                    &suite, &server_public_key));
     /* set the public key value. */
     memset(server_public_key.data, 0xF1, server_public_key.size);
 
     /* create a buffer for holding the key nonce. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_cipher_key_agreement_nonce(
-            &suite, &server_key_nonce));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_cipher_key_agreement_nonce(
+                    &suite, &server_key_nonce));
     /* set the key nonce value. */
     memset(server_key_nonce.data, 0xFE, server_key_nonce.size);
 
     /* create a buffer for holding the challenge nonce. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_cipher_key_agreement_nonce(
-            &suite, &server_challenge_nonce));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_cipher_key_agreement_nonce(
+                    &suite, &server_challenge_nonce));
     /* set the challenge nonce. */
     memset(server_challenge_nonce.data, 0xEC, server_challenge_nonce.size);
 
     /* create a buffer for holding the cr hmac. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_mac_authentication_code(
-            &suite, &server_cr_hmac, true));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_mac_authentication_code(
+                    &suite, &server_cr_hmac, true));
     /* set the cr hmac. */
     memset(server_cr_hmac.data, 0xE1, server_cr_hmac.size);
 
     /* encoding the handshake response should succeed. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vcblockchain_protocol_encode_resp_handshake_request(
-            &out, &suite, EXPECTED_OFFSET, EXPECTED_STATUS, &agent_id,
-            &server_public_key, &server_key_nonce, &server_challenge_nonce,
-            &server_cr_hmac));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vcblockchain_protocol_encode_resp_handshake_request(
+                    &out, &suite, EXPECTED_OFFSET, EXPECTED_STATUS, &agent_id,
+                    &server_public_key, &server_key_nonce,
+                    &server_challenge_nonce, &server_cr_hmac));
 
     /* decoding the handshake request response should succeed. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vcblockchain_protocol_decode_resp_handshake_request(
-            &resp, &suite, out.data, out.size));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vcblockchain_protocol_decode_resp_handshake_request(
+                    &resp, &suite, out.data, out.size));
 
     /* the request id should match what we expect. */
-    EXPECT_EQ(PROTOCOL_REQ_ID_HANDSHAKE_INITIATE, resp.request_id);
+    TEST_EXPECT(PROTOCOL_REQ_ID_HANDSHAKE_INITIATE == resp.request_id);
 
     /* the offset should match. */
-    EXPECT_EQ(EXPECTED_OFFSET, resp.offset);
+    TEST_EXPECT(EXPECTED_OFFSET == resp.offset);
 
     /* the status should match. */
-    EXPECT_EQ(EXPECTED_STATUS, resp.status);
+    TEST_EXPECT(EXPECTED_STATUS == resp.status);
 
     /* the protocol version should match what we expect. */
-    EXPECT_EQ(PROTOCOL_VERSION_0_1_DEMO, resp.protocol_version);
+    TEST_EXPECT(PROTOCOL_VERSION_0_1_DEMO == resp.protocol_version);
 
     /* the crypto suite should match what we expect. */
-    EXPECT_EQ(VCCRYPT_SUITE_VELO_V1, resp.crypto_suite);
+    TEST_EXPECT(VCCRYPT_SUITE_VELO_V1 == resp.crypto_suite);
 
     /* the agent id should match. */
-    EXPECT_EQ(0, memcmp(&agent_id, &resp.agent_id, sizeof(agent_id)));
+    TEST_EXPECT(0 == memcmp(&agent_id, &resp.agent_id, sizeof(agent_id)));
 
     /* the public key should match. */
-    ASSERT_TRUE(resp.server_public_key_set);
-    EXPECT_EQ(server_public_key.size, resp.server_public_key.size);
-    EXPECT_EQ(
-        0,
-        memcmp(
-            server_public_key.data, resp.server_public_key.data,
-            server_public_key.size));
+    TEST_ASSERT(resp.server_public_key_set);
+    TEST_EXPECT(server_public_key.size == resp.server_public_key.size);
+    TEST_EXPECT(
+        0
+            == memcmp(
+                    server_public_key.data, resp.server_public_key.data,
+                    server_public_key.size));
 
     /* the key nonce should match. */
-    ASSERT_TRUE(resp.server_key_nonce_set);
-    EXPECT_EQ(server_key_nonce.size, resp.server_key_nonce.size);
-    EXPECT_EQ(
-        0,
-        memcmp(
-            server_key_nonce.data, resp.server_key_nonce.data,
-            server_key_nonce.size));
+    TEST_ASSERT(resp.server_key_nonce_set);
+    TEST_EXPECT(server_key_nonce.size == resp.server_key_nonce.size);
+    TEST_EXPECT(
+        0
+            == memcmp(
+                    server_key_nonce.data, resp.server_key_nonce.data,
+                    server_key_nonce.size));
 
     /* the challenge nonce should match. */
-    ASSERT_TRUE(resp.server_challenge_nonce_set);
-    EXPECT_EQ(server_challenge_nonce.size, resp.server_challenge_nonce.size);
-    EXPECT_EQ(
-        0,
-        memcmp(
-            server_challenge_nonce.data, resp.server_challenge_nonce.data,
-            server_challenge_nonce.size));
+    TEST_ASSERT(resp.server_challenge_nonce_set);
+    TEST_EXPECT(
+        server_challenge_nonce.size == resp.server_challenge_nonce.size);
+    TEST_EXPECT(
+        0
+            == memcmp(
+                    server_challenge_nonce.data,
+                    resp.server_challenge_nonce.data,
+                    server_challenge_nonce.size));
 
     /* the cr hmac should match. */
-    ASSERT_TRUE(resp.server_cr_hmac_set);
-    EXPECT_EQ(server_cr_hmac.size, resp.server_cr_hmac.size);
-    EXPECT_EQ(
-        0,
-        memcmp(
-            server_cr_hmac.data, resp.server_cr_hmac.data,
-            server_cr_hmac.size));
+    TEST_ASSERT(resp.server_cr_hmac_set);
+    TEST_EXPECT(server_cr_hmac.size == resp.server_cr_hmac.size);
+    TEST_EXPECT(
+        0
+            == memcmp(
+                    server_cr_hmac.data, resp.server_cr_hmac.data,
+                    server_cr_hmac.size));
 
     /* clean up. */
     dispose((disposable_t*)&resp);
@@ -157,4 +161,3 @@ TEST(test_vcblockchain_protocol_decode_resp_handshake_request, basics)
     dispose((disposable_t*)&suite);
     dispose((disposable_t*)&alloc_opts);
 }
-#endif
