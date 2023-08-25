dc4fe659ad7259be9a633a218080842e28fe941b
Justin Handville <nanolith@gmail.com>
2023-08-24 21:22:43 -0400

    Converted unit tests to minunit.

--- test/protocol/test_vcblockchain_protocol_encode_req_artifact_first_txn_id_get.cpp
+++ test/protocol/test_vcblockchain_protocol_encode_req_artifact_first_txn_id_get.cpp
@@ -4,25 +4,24 @@
  *
  * Unit tests for encoding the artifact first txn id get request.
  *
- * \copyright 2020 Velo Payments, Inc.  All rights reserved.
+ * \copyright 2020-2023 Velo Payments, Inc.  All rights reserved.
  */
 
 #include <arpa/inet.h>
+#include <cstring>
+#include <minunit/minunit.h>
 #include <vcblockchain/error_codes.h>
 #include <vcblockchain/protocol/serialization.h>
 #include <vpr/allocator/malloc_allocator.h>
 
-/* DISABLED GTEST */
-#if 0
-
 using namespace std;
+
+TEST_SUITE(test_vcblockchain_protocol_encode_req_artifact_first_txn_id_get);
 
 /**
  * This method should perform null checks on its pointer parameters.
  */
-TEST(
-    test_vcblockchain_protocol_encode_req_artifact_first_txn_id_get,
-    parameter_check)
+TEST(parameter_check)
 {
     const uint32_t EXPECTED_OFFSET = 97;
     allocator_options_t alloc_opts;
@@ -35,18 +34,19 @@ TEST(
     malloc_allocator_options_init(&alloc_opts);
 
     /* This method performs null checks on pointer parameters. */
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_encode_req_artifact_first_txn_id_get(
-            nullptr, &alloc_opts, EXPECTED_OFFSET, &EXPECTED_ARTIFACT_ID));
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_encode_req_artifact_first_txn_id_get(
-            &buffer, nullptr, EXPECTED_OFFSET, &EXPECTED_ARTIFACT_ID));
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_encode_req_artifact_first_txn_id_get(
-            &buffer, &alloc_opts, EXPECTED_OFFSET, nullptr));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_encode_req_artifact_first_txn_id_get(
+                    nullptr, &alloc_opts, EXPECTED_OFFSET,
+                    &EXPECTED_ARTIFACT_ID));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_encode_req_artifact_first_txn_id_get(
+                    &buffer, nullptr, EXPECTED_OFFSET, &EXPECTED_ARTIFACT_ID));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_encode_req_artifact_first_txn_id_get(
+                    &buffer, &alloc_opts, EXPECTED_OFFSET, nullptr));
 
     /* clean up. */
     dispose((disposable_t*)&alloc_opts);
@@ -55,8 +55,7 @@ TEST(
 /**
  * If valid parameters are provided, this method encodes a request message.
  */
-TEST(
-    test_vcblockchain_protocol_encode_req_artifact_first_txn_id_get, happy_path)
+TEST(happy_path)
 {
     const uint32_t EXPECTED_OFFSET = 97;
     allocator_options_t alloc_opts;
@@ -72,29 +71,30 @@ TEST(
     buffer.data = nullptr; buffer.size = 0;
 
     /* This method performs null checks on pointer parameters. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vcblockchain_protocol_encode_req_artifact_first_txn_id_get(
-            &buffer, &alloc_opts, EXPECTED_OFFSET, &EXPECTED_ARTIFACT_ID));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vcblockchain_protocol_encode_req_artifact_first_txn_id_get(
+                    &buffer, &alloc_opts, EXPECTED_OFFSET,
+                    &EXPECTED_ARTIFACT_ID));
 
     /* compute the message size. */
     size_t message_size = 2 * sizeof(uint32_t) + 16;
 
     /* the buffer has been initialized. */
-    ASSERT_NE(nullptr, buffer.data);
-    ASSERT_EQ(message_size, buffer.size);
+    TEST_ASSERT(nullptr != buffer.data);
+    TEST_ASSERT(message_size == buffer.size);
 
     /* verify that the request id and offset is set correctly. */
     const uint32_t* u32arr = (const uint32_t*)buffer.data;
-    EXPECT_EQ(htonl(PROTOCOL_REQ_ID_ARTIFACT_FIRST_TXN_BY_ID_GET), u32arr[0]);
-    EXPECT_EQ(htonl(EXPECTED_OFFSET), u32arr[1]);
+    TEST_EXPECT(
+        htonl(PROTOCOL_REQ_ID_ARTIFACT_FIRST_TXN_BY_ID_GET) == u32arr[0]);
+    TEST_EXPECT(htonl(EXPECTED_OFFSET) == u32arr[1]);
 
     /* verify that the block id is set correctly. */
     const uint8_t* barr = (const uint8_t*)(u32arr + 2);
-    EXPECT_EQ(0, memcmp(barr, &EXPECTED_ARTIFACT_ID, 16));
+    TEST_EXPECT(0 == memcmp(barr, &EXPECTED_ARTIFACT_ID, 16));
 
     /* clean up. */
     dispose((disposable_t*)&buffer);
     dispose((disposable_t*)&alloc_opts);
 }
-#endif
