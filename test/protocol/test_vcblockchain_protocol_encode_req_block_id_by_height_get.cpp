833fbe0e829c64d582e5f88988e4e96682025d81
Justin Handville <nanolith@gmail.com>
2023-08-24 22:02:38 -0400

    Converted unit tests to minunit.

--- test/protocol/test_vcblockchain_protocol_encode_req_block_id_by_height_get.cpp
+++ test/protocol/test_vcblockchain_protocol_encode_req_block_id_by_height_get.cpp
@@ -4,26 +4,24 @@
  *
  * Unit tests for encoding the block id by height get request.
  *
- * \copyright 2021 Velo Payments, Inc.  All rights reserved.
+ * \copyright 2021-2023 Velo Payments, Inc.  All rights reserved.
  */
 
 #include <arpa/inet.h>
+#include <minunit/minunit.h>
 #include <vcblockchain/byteswap.h>
 #include <vcblockchain/error_codes.h>
 #include <vcblockchain/protocol/serialization.h>
 #include <vpr/allocator/malloc_allocator.h>
 
-/* DISABLED GTEST */
-#if 0
-
 using namespace std;
+
+TEST_SUITE(test_vcblockchain_protocol_encode_req_block_id_by_height_get);
 
 /**
  * This method should perform null checks on its pointer parameters.
  */
-TEST(
-    test_vcblockchain_protocol_encode_req_block_id_by_height_get,
-    parameter_check)
+TEST(parameter_check)
 {
     const uint32_t EXPECTED_OFFSET = 97;
     allocator_options_t alloc_opts;
@@ -34,14 +32,14 @@ TEST(
     malloc_allocator_options_init(&alloc_opts);
 
     /* This method performs null checks on pointer parameters. */
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_encode_req_block_id_by_height_get(
-            nullptr, &alloc_opts, EXPECTED_OFFSET, EXPECTED_HEIGHT));
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_encode_req_block_id_by_height_get(
-            &buffer, nullptr, EXPECTED_OFFSET, EXPECTED_HEIGHT));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_encode_req_block_id_by_height_get(
+                    nullptr, &alloc_opts, EXPECTED_OFFSET, EXPECTED_HEIGHT));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_encode_req_block_id_by_height_get(
+                    &buffer, nullptr, EXPECTED_OFFSET, EXPECTED_HEIGHT));
 
     /* clean up. */
     dispose((disposable_t*)&alloc_opts);
@@ -50,7 +48,7 @@ TEST(
 /**
  * If valid parameters are provided, this method encodes a request message.
  */
-TEST(test_vcblockchain_protocol_encode_req_block_id_by_height_get, happy_path)
+TEST(happy_path)
 {
     const uint32_t EXPECTED_OFFSET = 97;
     allocator_options_t alloc_opts;
@@ -64,29 +62,28 @@ TEST(test_vcblockchain_protocol_encode_req_block_id_by_height_get, happy_path)
     buffer.data = nullptr; buffer.size = 0;
 
     /* This method performs null checks on pointer parameters. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vcblockchain_protocol_encode_req_block_id_by_height_get(
-            &buffer, &alloc_opts, EXPECTED_OFFSET, EXPECTED_HEIGHT));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vcblockchain_protocol_encode_req_block_id_by_height_get(
+                    &buffer, &alloc_opts, EXPECTED_OFFSET, EXPECTED_HEIGHT));
 
     /* compute the message size. */
     size_t message_size = 2 * sizeof(uint32_t) + 8;
 
     /* the buffer has been initialized. */
-    ASSERT_NE(nullptr, buffer.data);
-    ASSERT_EQ(message_size, buffer.size);
+    TEST_ASSERT(nullptr != buffer.data);
+    TEST_ASSERT(message_size == buffer.size);
 
     /* verify that the request id and offset is set correctly. */
     const uint32_t* u32arr = (const uint32_t*)buffer.data;
-    EXPECT_EQ(htonl(PROTOCOL_REQ_ID_BLOCK_ID_BY_HEIGHT_GET), u32arr[0]);
-    EXPECT_EQ(htonl(EXPECTED_OFFSET), u32arr[1]);
+    TEST_EXPECT(htonl(PROTOCOL_REQ_ID_BLOCK_ID_BY_HEIGHT_GET) == u32arr[0]);
+    TEST_EXPECT(htonl(EXPECTED_OFFSET) == u32arr[1]);
 
     /* verify that the block id is set correctly. */
     const uint64_t* height = (const uint64_t*)(u32arr + 2);
-    EXPECT_EQ(htonll(*height), EXPECTED_HEIGHT);
+    TEST_EXPECT(htonll(*height) == EXPECTED_HEIGHT);
 
     /* clean up. */
     dispose((disposable_t*)&buffer);
     dispose((disposable_t*)&alloc_opts);
 }
-#endif
