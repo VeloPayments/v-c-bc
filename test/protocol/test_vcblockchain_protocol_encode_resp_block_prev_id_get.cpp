7144b3fc8cf2f70adbfbd73219db903cd6b3cbac
Justin Handville <nanolith@gmail.com>
2023-08-25 21:48:09 -0400

    Converted unit tests to minunit.

--- test/protocol/test_vcblockchain_protocol_encode_resp_block_prev_id_get.cpp
+++ test/protocol/test_vcblockchain_protocol_encode_resp_block_prev_id_get.cpp
@@ -4,23 +4,24 @@
  *
  * Unit tests for encoding the block prev id get response.
  *
- * \copyright 2021 Velo Payments, Inc.  All rights reserved.
+ * \copyright 2021-2023 Velo Payments, Inc.  All rights reserved.
  */
 
 #include <arpa/inet.h>
+#include <cstring>
+#include <minunit/minunit.h>
 #include <vcblockchain/error_codes.h>
 #include <vcblockchain/protocol/serialization.h>
 #include <vpr/allocator/malloc_allocator.h>
 
-/* DISABLED GTEST */
-#if 0
-
 using namespace std;
+
+TEST_SUITE(test_vcblockchain_protocol_encode_resp_block_prev_id_get);
 
 /**
  * This method should perform null checks on its pointer parameters.
  */
-TEST(test_vcblockchain_protocol_encode_resp_block_prev_id_get, parameters)
+TEST(parameters)
 {
     const uint32_t EXPECTED_OFFSET = 26;
     const uint32_t EXPECTED_STATUS = 11;
@@ -34,28 +35,28 @@ TEST(test_vcblockchain_protocol_encode_resp_block_prev_id_get, parameters)
     malloc_allocator_options_init(&alloc_opts);
 
     /* this method performs null checks on pointer parameters. */
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_encode_resp_block_prev_id_get(
-            nullptr, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS,
-            &EXPECTED_NEXT_BLOCK_ID));
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_encode_resp_block_prev_id_get(
-            &buffer, nullptr, EXPECTED_OFFSET, EXPECTED_STATUS,
-            &EXPECTED_NEXT_BLOCK_ID));
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_encode_resp_block_prev_id_get(
-            &buffer, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS,
-            nullptr));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_encode_resp_block_prev_id_get(
+                    nullptr, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS,
+                    &EXPECTED_NEXT_BLOCK_ID));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_encode_resp_block_prev_id_get(
+                    &buffer, nullptr, EXPECTED_OFFSET, EXPECTED_STATUS,
+                    &EXPECTED_NEXT_BLOCK_ID));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_encode_resp_block_prev_id_get(
+                    &buffer, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS,
+                    nullptr));
 
     /* clean up. */
     dispose((disposable_t*)&alloc_opts);
 }
 
 /* This method should encode the response message. */
-TEST(test_vcblockchain_protocol_encode_resp_block_prev_id_get, happy_path)
+TEST(happy_path)
 {
     const uint32_t EXPECTED_OFFSET = 26;
     const uint32_t EXPECTED_STATUS = 11;
@@ -72,29 +73,29 @@ TEST(test_vcblockchain_protocol_encode_resp_block_prev_id_get, happy_path)
     buffer.data = nullptr; buffer.size = 0;
 
     /* this method should succeed. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vcblockchain_protocol_encode_resp_block_prev_id_get(
-            &buffer, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS,
-            &EXPECTED_BLOCK_ID));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vcblockchain_protocol_encode_resp_block_prev_id_get(
+                    &buffer, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS,
+                    &EXPECTED_BLOCK_ID));
 
     /* the buffer should not be null. */
-    ASSERT_NE(nullptr, buffer.data);
-    ASSERT_EQ(3 * sizeof(uint32_t) + 16, buffer.size);
+    TEST_ASSERT(nullptr != buffer.data);
+    TEST_ASSERT(3 * sizeof(uint32_t) + 16 == buffer.size);
 
     /* check the integer values. */
     uint32_t* uarr = (uint32_t*)buffer.data;
-    EXPECT_EQ(htonl(PROTOCOL_REQ_ID_BLOCK_ID_GET_PREV), uarr[0]);
-    EXPECT_EQ(htonl(EXPECTED_STATUS), uarr[1]);
-    EXPECT_EQ(htonl(EXPECTED_OFFSET), uarr[2]);
+    TEST_EXPECT(htonl(PROTOCOL_REQ_ID_BLOCK_ID_GET_PREV) == uarr[0]);
+    TEST_EXPECT(htonl(EXPECTED_STATUS) == uarr[1]);
+    TEST_EXPECT(htonl(EXPECTED_OFFSET) == uarr[2]);
 
     /* check the uuid. */
     uint8_t* barr = (uint8_t*)buffer.data;
     barr += 3 * sizeof(uint32_t);
-    EXPECT_EQ(0, memcmp(barr, &EXPECTED_BLOCK_ID, sizeof(EXPECTED_BLOCK_ID)));
+    TEST_EXPECT(
+        0 == memcmp(barr, &EXPECTED_BLOCK_ID, sizeof(EXPECTED_BLOCK_ID)));
 
     /* clean up. */
     dispose((disposable_t*)&buffer);
     dispose((disposable_t*)&alloc_opts);
 }
-#endif
