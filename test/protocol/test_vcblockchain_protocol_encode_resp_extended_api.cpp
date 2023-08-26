0eaf02a2e002749779aa048606564e4ad8c18a78
Justin Handville <nanolith@gmail.com>
2023-08-25 21:55:48 -0400

    Converted unit tests to minunit.

--- test/protocol/test_vcblockchain_protocol_encode_resp_extended_api.cpp
+++ test/protocol/test_vcblockchain_protocol_encode_resp_extended_api.cpp
@@ -4,23 +4,24 @@
  *
  * Unit tests for encoding an extended api response.
  *
- * \copyright 2022 Velo Payments, Inc.  All rights reserved.
+ * \copyright 2022-2023 Velo Payments, Inc.  All rights reserved.
  */
 
 #include <arpa/inet.h>
+#include <cstring>
+#include <minunit/minunit.h>
 #include <vcblockchain/error_codes.h>
 #include <vcblockchain/protocol/serialization.h>
 #include <vpr/allocator/malloc_allocator.h>
 
-/* DISABLED GTEST */
-#if 0
-
 using namespace std;
+
+TEST_SUITE(test_vcblockchain_protocol_encode_resp_extended_api);
 
 /**
  * This method should perform null checks on its pointer parameters.
  */
-TEST(test_vcblockchain_protocol_encode_resp_extended_api, parameter_checks)
+TEST(parameter_checks)
 {
     const uint32_t EXPECTED_OFFSET = 26;
     const uint32_t EXPECTED_STATUS = 11;
@@ -32,21 +33,21 @@ TEST(test_vcblockchain_protocol_encode_resp_extended_api, parameter_checks)
     malloc_allocator_options_init(&alloc_opts);
 
     /* this method performs null checks on pointer parameters. */
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_encode_resp_extended_api(
-            nullptr, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS,
-            &response_body));
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_encode_resp_extended_api(
-            &buffer, nullptr, EXPECTED_OFFSET, EXPECTED_STATUS,
-            &response_body));
-    EXPECT_EQ(
-        VCBLOCKCHAIN_ERROR_INVALID_ARG,
-        vcblockchain_protocol_encode_resp_extended_api(
-            &buffer, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS,
-            nullptr));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_encode_resp_extended_api(
+                    nullptr, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS,
+                    &response_body));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_encode_resp_extended_api(
+                    &buffer, nullptr, EXPECTED_OFFSET, EXPECTED_STATUS,
+                    &response_body));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_ERROR_INVALID_ARG
+            == vcblockchain_protocol_encode_resp_extended_api(
+                    &buffer, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS,
+                    nullptr));
 
     /* clean up. */
     dispose((disposable_t*)&alloc_opts);
@@ -55,7 +56,7 @@ TEST(test_vcblockchain_protocol_encode_resp_extended_api, parameter_checks)
 /**
  * This method should encode the response message.
  */
-TEST(test_vcblockchain_protocol_encode_resp_extended_api, happy_path)
+TEST(happy_path)
 {
     const uint32_t EXPECTED_OFFSET = 26;
     const uint32_t EXPECTED_STATUS = 11;
@@ -67,37 +68,36 @@ TEST(test_vcblockchain_protocol_encode_resp_extended_api, happy_path)
     malloc_allocator_options_init(&alloc_opts);
 
     /* create a dummy buffer. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_buffer_init(&response_body, &alloc_opts, 32));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_buffer_init(&response_body, &alloc_opts, 32));
     memset(response_body.data, 0xde, response_body.size);
 
     /* precondition: buffer is nulled out. */
     buffer.data = nullptr; buffer.size = 0;
 
     /* this method should succeed. */
-    EXPECT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vcblockchain_protocol_encode_resp_extended_api(
-            &buffer, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS,
-            &response_body));
+    TEST_EXPECT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vcblockchain_protocol_encode_resp_extended_api(
+                    &buffer, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS,
+                    &response_body));
 
     /* the buffer should not be null. */
-    ASSERT_NE(nullptr, buffer.data);
-    ASSERT_EQ(3 * sizeof(uint32_t) + response_body.size, buffer.size);
+    TEST_ASSERT(nullptr != buffer.data);
+    TEST_ASSERT(3 * sizeof(uint32_t) + response_body.size == buffer.size);
 
     /* check the integer values. */
     const uint32_t* uarr = (const uint32_t*)buffer.data;
-    EXPECT_EQ(PROTOCOL_REQ_ID_EXTENDED_API_SENDRECV, ntohl(uarr[0]));
-    EXPECT_EQ(EXPECTED_STATUS, ntohl(uarr[1]));
-    EXPECT_EQ(EXPECTED_OFFSET, ntohl(uarr[2]));
+    TEST_EXPECT(PROTOCOL_REQ_ID_EXTENDED_API_SENDRECV == ntohl(uarr[0]));
+    TEST_EXPECT(EXPECTED_STATUS == ntohl(uarr[1]));
+    TEST_EXPECT(EXPECTED_OFFSET == ntohl(uarr[2]));
 
     /* check the body. */
-    EXPECT_EQ(0, memcmp(uarr + 3, response_body.data, response_body.size));
+    TEST_EXPECT(0 == memcmp(uarr + 3, response_body.data, response_body.size));
 
     /* clean up. */
     dispose((disposable_t*)&buffer);
     dispose((disposable_t*)&response_body);
     dispose((disposable_t*)&alloc_opts);
 }
-#endif
