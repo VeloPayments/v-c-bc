92266b3f81b0ad1db5fcceeaedb5bf46fee8e844
Justin Handville <nanolith@gmail.com>
2023-08-26 04:16:14 -0400

    Converted unit tests to minunit.

--- test/protocol/test_vcblockchain_protocol_recvresp_handshake_request.cpp
+++ test/protocol/test_vcblockchain_protocol_recvresp_handshake_request.cpp
@@ -3,10 +3,12 @@
  *
  * Unit tests for receiving the handshake request from a server socket.
  *
- * \copyright 2020 Velo Payments, Inc.  All rights reserved.
+ * \copyright 2020-2023 Velo Payments, Inc.  All rights reserved.
  */
 
 #include <arpa/inet.h>
+#include <cstring>
+#include <minunit/minunit.h>
 #include <vcblockchain/error_codes.h>
 #include <vcblockchain/protocol.h>
 #include <vcblockchain/protocol/serialization.h>
@@ -14,19 +16,18 @@
 
 #include "../dummy_psock.h"
 
-/* DISABLED GTEST */
-#if 0
-
 using namespace std;
 
 RCPR_IMPORT_allocator_as(rcpr);
 RCPR_IMPORT_psock;
 RCPR_IMPORT_resource;
 
+TEST_SUITE(test_vcblockchain_protocol_recvresp_handshake_request);
+
 /**
  * Test the happy path.
  */
-TEST(test_vcblockchain_protocol_recvresp_handshake_request, happy_path)
+TEST(happy_path)
 {
     psock* sock;
     rcpr_allocator* alloc;
@@ -63,159 +64,164 @@ TEST(test_vcblockchain_protocol_recvresp_handshake_request, happy_path)
     malloc_allocator_options_init(&alloc_opts);
 
     /* create an RCPR allocator instance. */
-    ASSERT_EQ(STATUS_SUCCESS, rcpr_malloc_allocator_create(&alloc));
+    TEST_ASSERT(STATUS_SUCCESS == rcpr_malloc_allocator_create(&alloc));
 
     /* create the crypto suite. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_options_init(&suite, &alloc_opts, VCCRYPT_SUITE_VELO_V1));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_options_init(
+                    &suite, &alloc_opts, VCCRYPT_SUITE_VELO_V1));
 
     /* create prng instance. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_prng_init(&suite, &prng));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_prng_init(&suite, &prng));
 
     /* create cipher key agreement instance. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_cipher_key_agreement_init(
-            &suite, &cipher_key_agreement));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_cipher_key_agreement_init(
+                    &suite, &cipher_key_agreement));
 
     /* create client private key buffer. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_cipher_key_agreement_private_key(
-            &suite, &client_privkey));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_cipher_key_agreement_private_key(
+                    &suite, &client_privkey));
 
     /* create client public key buffer. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_cipher_key_agreement_public_key(
-            &suite, &client_pubkey));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_cipher_key_agreement_public_key(
+                    &suite, &client_pubkey));
 
     /* create client keypair. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_key_agreement_keypair_create(
-            &cipher_key_agreement, &client_privkey, &client_pubkey));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_key_agreement_keypair_create(
+                    &cipher_key_agreement, &client_privkey, &client_pubkey));
 
     /* create server private key buffer. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_cipher_key_agreement_private_key(
-            &suite, &server_privkey));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_cipher_key_agreement_private_key(
+                    &suite, &server_privkey));
 
     /* create server public key buffer. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_cipher_key_agreement_public_key(
-            &suite, &server_pubkey));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_cipher_key_agreement_public_key(
+                    &suite, &server_pubkey));
 
     /* create server keypair. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_key_agreement_keypair_create(
-            &cipher_key_agreement, &server_privkey, &server_pubkey));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_key_agreement_keypair_create(
+                    &cipher_key_agreement, &server_privkey, &server_pubkey));
 
     /* create client key nonce buffer. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_cipher_key_agreement_nonce(
-            &suite, &client_key_nonce));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_cipher_key_agreement_nonce(
+                    &suite, &client_key_nonce));
 
     /* create client key nonce. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_prng_read(&prng, &client_key_nonce, client_key_nonce.size));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_prng_read(
+                    &prng, &client_key_nonce, client_key_nonce.size));
 
     /* create client challenge nonce buffer. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_cipher_key_agreement_nonce(
-            &suite, &client_challenge_nonce));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_cipher_key_agreement_nonce(
+                    &suite, &client_challenge_nonce));
 
     /* create client challenge nonce. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_prng_read(
-            &prng, &client_challenge_nonce, client_challenge_nonce.size));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_prng_read(
+                    &prng, &client_challenge_nonce,
+                    client_challenge_nonce.size));
 
     /* create server key nonce buffer. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_cipher_key_agreement_nonce(
-            &suite, &server_key_nonce));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_cipher_key_agreement_nonce(
+                    &suite, &server_key_nonce));
 
     /* create server key nonce. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_prng_read(
-            &prng, &server_key_nonce, server_key_nonce.size));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_prng_read(
+                    &prng, &server_key_nonce, server_key_nonce.size));
 
     /* create server challenge nonce buffer. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_cipher_key_agreement_nonce(
-            &suite, &server_challenge_nonce));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_cipher_key_agreement_nonce(
+                    &suite, &server_challenge_nonce));
 
     /* create server challenge nonce. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_prng_read(
-            &prng, &server_challenge_nonce, server_challenge_nonce.size));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_prng_read(
+                    &prng, &server_challenge_nonce,
+                    server_challenge_nonce.size));
 
     /* create server cr mac buffer. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_mac_authentication_code(
-            &suite, &server_cr_hmac, true));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_mac_authentication_code(
+                    &suite, &server_cr_hmac, true));
 
     /* clear it for now. */
     memset(server_cr_hmac.data, 0, server_cr_hmac.size);
 
     /* build response packet. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vcblockchain_protocol_encode_resp_handshake_request(
-            &out, &suite, EXPECTED_OFFSET, EXPECTED_STATUS, &EXPECTED_AGENT_ID,
-            &server_pubkey, &server_key_nonce, &server_challenge_nonce,
-            &server_cr_hmac));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vcblockchain_protocol_encode_resp_handshake_request(
+                    &out, &suite, EXPECTED_OFFSET, EXPECTED_STATUS,
+                    &EXPECTED_AGENT_ID, &server_pubkey, &server_key_nonce,
+                    &server_challenge_nonce, &server_cr_hmac));
 
     /* create shared secret buffer. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vccrypt_suite_buffer_init_for_cipher_key_agreement_shared_secret(
-            &suite, &shared_secret));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vccrypt_suite_buffer_init_for_cipher_key_agreement_shared_secret(
+                    &suite, &shared_secret));
 
     /* derive shared secret. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vccrypt_key_agreement_short_term_secret_create(
-            &cipher_key_agreement, &client_privkey, &server_pubkey,
-            &server_key_nonce, &client_key_nonce, &shared_secret));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vccrypt_key_agreement_short_term_secret_create(
+                    &cipher_key_agreement, &client_privkey, &server_pubkey,
+                    &server_key_nonce, &client_key_nonce, &shared_secret));
 
     /* create mac instance. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vccrypt_suite_mac_short_init(&suite, &mac, &shared_secret));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vccrypt_suite_mac_short_init(&suite, &mac, &shared_secret));
 
     /* mac initial packet bytes. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vccrypt_mac_digest(
-            &mac, (const uint8_t*)out.data, out.size - server_cr_hmac.size));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vccrypt_mac_digest(
+                    &mac, (const uint8_t*)out.data,
+                    out.size - server_cr_hmac.size));
 
     /* mac client challenge nonce. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vccrypt_mac_digest(
-            &mac, (const uint8_t*)client_challenge_nonce.data,
-            client_challenge_nonce.size));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vccrypt_mac_digest(
+                    &mac, (const uint8_t*)client_challenge_nonce.data,
+                    client_challenge_nonce.size));
 
     /* finalize. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vccrypt_mac_finalize(&mac, &server_cr_hmac));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vccrypt_mac_finalize(&mac, &server_cr_hmac));
 
     /* copy bytes to response packet. */
     memcpy(
@@ -224,94 +230,100 @@ TEST(test_vcblockchain_protocol_recvresp_handshake_request, happy_path)
 
     /* build dummy socket. */
     int state = 0;
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        dummy_psock_create(
-            &sock, alloc,
-            [&](psock*, void* buffer, size_t* size) -> int {
-                uint32_t type = ntohl(PSOCK_BOXED_TYPE_DATA);
-                switch (state)
-                {
-                    /* read the type. */
-                    case 0:
-                        if (*size != sizeof(uint32_t))
-                            return VCBLOCKCHAIN_ERROR_SSOCK_READ;
-                        memcpy(buffer, &type, sizeof(type));
-                        ++state;
-                        return VCBLOCKCHAIN_STATUS_SUCCESS;
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == dummy_psock_create(
+                    &sock, alloc,
+                    [&](psock*, void* buffer, size_t* size) -> int {
+                        uint32_t type = ntohl(PSOCK_BOXED_TYPE_DATA);
+                        switch (state)
+                        {
+                            /* read the type. */
+                            case 0:
+                                if (*size != sizeof(uint32_t))
+                                    return VCBLOCKCHAIN_ERROR_SSOCK_READ;
+                                memcpy(buffer, &type, sizeof(type));
+                                ++state;
+                                return VCBLOCKCHAIN_STATUS_SUCCESS;
 
-                    case 1:
-                        if (*size != sizeof(uint32_t))
-                            return VCBLOCKCHAIN_ERROR_SSOCK_READ;
-                        *((uint32_t*)buffer) = htonl(out.size);
-                        ++state;
-                        return VCBLOCKCHAIN_STATUS_SUCCESS;
+                            case 1:
+                                if (*size != sizeof(uint32_t))
+                                    return VCBLOCKCHAIN_ERROR_SSOCK_READ;
+                                *((uint32_t*)buffer) = htonl(out.size);
+                                ++state;
+                                return VCBLOCKCHAIN_STATUS_SUCCESS;
 
-                    case 2:
-                        if (*size != out.size)
-                            return VCBLOCKCHAIN_ERROR_SSOCK_READ;
-                        memcpy(buffer, out.data, out.size);
-                        ++state;
-                        return VCBLOCKCHAIN_STATUS_SUCCESS;
+                            case 2:
+                                if (*size != out.size)
+                                    return VCBLOCKCHAIN_ERROR_SSOCK_READ;
+                                memcpy(buffer, out.data, out.size);
+                                ++state;
+                                return VCBLOCKCHAIN_STATUS_SUCCESS;
 
-                    default:
-                        return VCBLOCKCHAIN_ERROR_SSOCK_READ;
-                }
-            },
-            [&](psock*, const void*, size_t*) -> int {
-                return VCBLOCKCHAIN_ERROR_SSOCK_WRITE;
-            }));
+                            default:
+                                return VCBLOCKCHAIN_ERROR_SSOCK_READ;
+                        }
+                    },
+                    [&](psock*, const void*, size_t*) -> int {
+                        return VCBLOCKCHAIN_ERROR_SSOCK_WRITE;
+                    }));
 
     /* read the response. */
-    ASSERT_EQ(
-        VCBLOCKCHAIN_STATUS_SUCCESS,
-        vcblockchain_protocol_recvresp_handshake_request(
-            sock, alloc, &suite, &agent_id, &read_server_pubkey,
-            &client_privkey, &client_key_nonce, &client_challenge_nonce,
-            &read_server_challenge_nonce, &read_shared_secret, &offset,
-            &status));
+    TEST_ASSERT(
+        VCBLOCKCHAIN_STATUS_SUCCESS
+            == vcblockchain_protocol_recvresp_handshake_request(
+                    sock, alloc, &suite, &agent_id, &read_server_pubkey,
+                    &client_privkey, &client_key_nonce, &client_challenge_nonce,
+                    &read_server_challenge_nonce, &read_shared_secret, &offset,
+                    &status));
 
     /* verify the offset. */
-    EXPECT_EQ(EXPECTED_OFFSET, offset);
+    TEST_EXPECT(EXPECTED_OFFSET == offset);
 
     /* verify the status. */
-    EXPECT_EQ(EXPECTED_STATUS, status);
+    TEST_EXPECT(EXPECTED_STATUS == status);
 
     /* verify the read agent id. */
-    ASSERT_EQ(
-        0,
-        memcmp(&EXPECTED_AGENT_ID, &agent_id, sizeof(EXPECTED_AGENT_ID)));
+    TEST_ASSERT(
+        0
+            == memcmp(
+                    &EXPECTED_AGENT_ID, &agent_id, sizeof(EXPECTED_AGENT_ID)));
 
     /* verify the read server public key. */
-    ASSERT_NE(nullptr, read_server_pubkey.data);
-    ASSERT_EQ(server_pubkey.size, read_server_pubkey.size);
-    EXPECT_EQ(
-        0,
-        memcmp(
-            server_pubkey.data, read_server_pubkey.data, server_pubkey.size));
+    TEST_ASSERT(nullptr != read_server_pubkey.data);
+    TEST_ASSERT(server_pubkey.size == read_server_pubkey.size);
+    TEST_EXPECT(
+        0
+            == memcmp(
+                    server_pubkey.data, read_server_pubkey.data,
+                    server_pubkey.size));
 
     /* verify the read server challenge nonce. */
-    ASSERT_NE(nullptr, read_server_challenge_nonce.data);
-    ASSERT_EQ(server_challenge_nonce.size, read_server_challenge_nonce.size);
-    EXPECT_EQ(
-        0,
-        memcmp(
-            server_challenge_nonce.data, read_server_challenge_nonce.data,
-            server_challenge_nonce.size));
+    TEST_ASSERT(nullptr != read_server_challenge_nonce.data);
+    TEST_ASSERT(
+        server_challenge_nonce.size == read_server_challenge_nonce.size);
+    TEST_EXPECT(
+        0
+            == memcmp(
+                    server_challenge_nonce.data,
+                    read_server_challenge_nonce.data,
+                    server_challenge_nonce.size));
 
     /* verify the read shared secret. */
-    ASSERT_NE(nullptr, read_shared_secret.data);
-    ASSERT_EQ(shared_secret.size, read_shared_secret.size);
-    EXPECT_EQ(
-        0,
-        memcmp(
-            shared_secret.data, read_shared_secret.data, shared_secret.size));
+    TEST_ASSERT(nullptr != read_shared_secret.data);
+    TEST_ASSERT(shared_secret.size == read_shared_secret.size);
+    TEST_EXPECT(
+        0
+            == memcmp(
+                    shared_secret.data, read_shared_secret.data,
+                    shared_secret.size));
 
     /* clean up. */
-    ASSERT_EQ(STATUS_SUCCESS, resource_release(psock_resource_handle(sock)));
-    ASSERT_EQ(
-        STATUS_SUCCESS,
-        resource_release(rcpr_allocator_resource_handle(alloc)));
+    TEST_ASSERT(
+        STATUS_SUCCESS == resource_release(psock_resource_handle(sock)));
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == resource_release(rcpr_allocator_resource_handle(alloc)));
     dispose((disposable_t*)&prng);
     dispose((disposable_t*)&cipher_key_agreement);
     dispose((disposable_t*)&mac);
@@ -332,4 +344,3 @@ TEST(test_vcblockchain_protocol_recvresp_handshake_request, happy_path)
     dispose((disposable_t*)&suite);
     dispose((disposable_t*)&alloc_opts);
 }
-#endif
