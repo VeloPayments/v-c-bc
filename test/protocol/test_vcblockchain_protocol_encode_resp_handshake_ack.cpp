7c45d64c24567a2829f2fed271182fc6bb0731bc
Justin Handville <nanolith@gmail.com>
2023-08-25 22:11:14 -0400

    Converted unit tests to minunit.

--- test/protocol/test_vcblockchain_protocol_encode_resp_handshake_ack.cpp
+++ test/protocol/test_vcblockchain_protocol_encode_resp_handshake_ack.cpp
@@ -3,23 +3,24 @@
  *
  * Unit tests for encoding the response portion of the handshake ack.
  *
- * \copyright 2020 Velo Payments, Inc.  All rights reserved.
+ * \copyright 2020-2023 Velo Payments, Inc.  All rights reserved.
  */
 
 #include <arpa/inet.h>
+#include <cstring>
+#include <minunit/minunit.h>
 #include <vcblockchain/protocol/data.h>
 #include <vcblockchain/protocol/serialization.h>
 #include <vpr/allocator/malloc_allocator.h>
 
-/* DISABLED GTEST */
-#if 0
-
 using namespace std;
+
+TEST_SUITE(test_vcblockchain_protocol_encode_resp_handshake_ack);
 
 /**
  * Test the basics of the encoding.
  */
-TEST(test_vcblockchain_protocol_encode_resp_handshake_ack, basics)
+TEST(basics)
 {
     allocator_options_t alloc_opts;
     vccrypt_buffer_t out;
@@ -34,14 +35,14 @@ TEST(test_vcblockchain_protocol_encode_resp_handshake_ack, basics)
     out.size = 0U;
 
     /* encoding the handshake ack should succeed. */
-    ASSERT_EQ(
-        VCCRYPT_STATUS_SUCCESS,
-        vcblockchain_protocol_encode_resp_handshake_ack(
-            &out, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS));
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS
+            == vcblockchain_protocol_encode_resp_handshake_ack(
+                    &out, &alloc_opts, EXPECTED_OFFSET, EXPECTED_STATUS));
 
     /* the out data and size should be set. */
-    ASSERT_NE(nullptr, out.data);
-    ASSERT_NE(0U, out.size);
+    TEST_ASSERT(nullptr != out.data);
+    TEST_ASSERT(0U != out.size);
 
     /* get a byte pointer to the output buffer. */
     const uint8_t* buf = (const uint8_t*)out.data;
@@ -49,26 +50,25 @@ TEST(test_vcblockchain_protocol_encode_resp_handshake_ack, basics)
 
     /* the first four bytes should be the request id. */
     uint32_t net_request;
-    ASSERT_GE(size, sizeof(net_request));
+    TEST_ASSERT(size >= sizeof(net_request));
     memcpy(&net_request, buf, sizeof(net_request));
-    EXPECT_EQ(PROTOCOL_REQ_ID_HANDSHAKE_ACKNOWLEDGE, ntohl(net_request));
+    TEST_EXPECT(PROTOCOL_REQ_ID_HANDSHAKE_ACKNOWLEDGE == ntohl(net_request));
     buf += sizeof(net_request); size -= sizeof(net_request);
 
     /* then comes the status. */
     uint32_t net_status;
-    ASSERT_GE(size, sizeof(net_status));
+    TEST_ASSERT(size >= sizeof(net_status));
     memcpy(&net_status, buf, sizeof(net_status));
-    EXPECT_EQ(EXPECTED_STATUS, ntohl(net_status));
+    TEST_EXPECT(EXPECTED_STATUS == ntohl(net_status));
     buf += sizeof(net_status); size -= sizeof(net_status);
 
     /* then comes the offset. */
     uint32_t net_offset;
-    ASSERT_EQ(size, sizeof(net_offset));
+    TEST_ASSERT(size == sizeof(net_offset));
     memcpy(&net_offset, buf, sizeof(net_offset));
-    EXPECT_EQ(EXPECTED_OFFSET, ntohl(net_offset));
+    TEST_EXPECT(EXPECTED_OFFSET == ntohl(net_offset));
 
     /* clean up. */
     dispose((disposable_t*)&out);
     dispose((disposable_t*)&alloc_opts);
 }
-#endif
